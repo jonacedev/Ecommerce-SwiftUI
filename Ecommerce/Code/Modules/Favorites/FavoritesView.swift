68eb91c4d9b9b068717faa77c534c08ea8b6c045
Jonathan <jonacedev@gmail.com>
2024-04-05 22:39:13 +0200

    Add logo and refactor

--- Ecommerce/Code/Modules/Favorites/FavoritesView.swift
+++ Ecommerce/Code/Modules/Favorites/FavoritesView.swift
@@ -12,21 +12,40 @@ struct FavoritesView: View {
 
     @ViewBuilder private func content() -> some View {
         ScrollView {
-            LazyVGrid(columns: [GridItem(.fixed(170)), GridItem(.fixed(170))]) {
-                ForEach(0..<productsData.favoritesProducts().count, id: \.self) { index in
-                    HomeGridCell(product: productsData.favoritesProducts()[index], favoritePressed: { favorite in
-                        removeFromFavorites(favIndex: index)
-                   })
-                   .onTapGesture {
-                       viewModel.goToDetail(product: productsData.favoritesProducts()[index])
+            if !productsData.favoritesProducts().isEmpty {
+                LazyVGrid(columns: [GridItem(.fixed(170)), GridItem(.fixed(170))]) {
+                    ForEach(0..<productsData.favoritesProducts().count, id: \.self) { index in
+                        HomeGridCell(product: productsData.favoritesProducts()[index], favoritePressed: { favorite in
+                            removeFromFavorites(favIndex: index)
+                       })
+                       .onTapGesture {
+                           viewModel.goToDetail(product: productsData.favoritesProducts()[index])
+                       }
                    }
                }
-           }
-            .padding(.bottom, 70)
+                .padding(.bottom, 70)
+            } else {
+                vwNoResults()
+            }
+           
        }
         .scrollIndicators(.hidden)
         .padding(.horizontal, 20)
         .padding(.top, 20)
+    }
+    
+    @ViewBuilder private func vwNoResults() -> some View {
+        HStack() {
+            Text("favorites_empty_list".localized)
+                .foregroundStyle(.white)
+                .font(.subheadline)
+                .padding(25)
+        }
+        .listRowSeparator(.hidden)
+        .frame(maxWidth: .infinity)
+        .background(Color.primaryApp.opacity(0.95))
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+        .padding(.top, 30)
     }
     
     func removeFromFavorites(favIndex: Int) {
