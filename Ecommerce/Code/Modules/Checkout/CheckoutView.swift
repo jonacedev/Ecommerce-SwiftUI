d6a6b815c033219d9d6a31fa0b886b73e13580bb
Jonathan <jonacedev@gmail.com>
2024-04-05 11:05:42 +0200

    Empty view when cart is empty

--- Ecommerce/Code/Modules/Checkout/CheckoutView.swift
+++ Ecommerce/Code/Modules/Checkout/CheckoutView.swift
@@ -14,38 +14,38 @@ struct CheckoutView: View {
     @ViewBuilder private func content() -> some View {
         VStack {
             List {
-                ForEach(productsData.productsCart, id: \.self) { product in
-                    CheckoutCell(product: product, updateProduct: { newPrice, newItemsAmount in
-                        productsData.updateProductCart(id: product.id, newPrice: newPrice, newItemsAmount: newItemsAmount)
-                    })
-                    .listRowSeparator(.hidden)
-                }
-                .onDelete(perform: deleteItem)
-                
                 if !productsData.productsCart.isEmpty {
                     
-                    checkoutDetails()
+                    ForEach(productsData.productsCart, id: \.self) { product in
+                        CheckoutCell(product: product, updateProduct: { newPrice, newItemsAmount in
+                            productsData.updateProductCart(id: product.id, newPrice: newPrice, newItemsAmount: newItemsAmount)
+                        })
+                        .listRowSeparator(.hidden)
+                    }
+                    .onDelete(perform: deleteItem)
+                    
+                    vwCheckoutDetails()
+                    
                     BaseButton(style: .primary, text: "Pay", action: {
                        
                     })
-                    .clipShape(Capsule())
                     .listRowSeparator(.hidden)
+                    .clipShape(Capsule())
                     .buttonStyle(.plain)
                     .padding(.top, 20)
                     .padding(.bottom, 70)
+                    
+                } else {
+                   vwNoResults()
                 }
             }
-          
             .listRowBackground(Color.clear)
             .listStyle(.plain)
             .padding(.top, 20)
         }
-        .onAppear {
-           
-        }
     }
     
-    @ViewBuilder private func checkoutDetails() -> some View {
+    @ViewBuilder private func vwCheckoutDetails() -> some View {
         VStack(alignment: .leading, spacing: 20) {
             Text("Shipping information")
                 .font(.system(size: 18).bold())
@@ -84,8 +84,18 @@ struct CheckoutView: View {
                 Text("0.0€")
                     .font(.headline)
             }
-           
         }
+    }
+    
+    @ViewBuilder private func vwNoResults() -> some View {
+        HStack() {
+            Text("There is still nothing in the cart")
+                .padding()
+        }
+        .listRowSeparator(.hidden)
+        .frame(maxWidth: .infinity)
+        .background(Color.gray.opacity(0.1))
+        .clipShape(RoundedRectangle(cornerRadius: 8))
     }
     
     func deleteItem(at offsets: IndexSet) {
